0846da1db84d4d3a90f8e790c4cdea39e5a9e608
Anthony Gold <anthgold@gmail.com>
2016-03-25 09:54:21 -0700

    adds quiz questions and radio buttons framework

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,40 @@
 $(document).ready(function() {
-  $("#blank form").submit(function(event) {
-
-    var nameInput = $("input#name").val();
-
-    $(".name").text(nameInput);
-
-    $("#letter").show();
-
+  // $("#blank form").submit(function(event) {
+  //
+  //   var nameInput = $("input#name").val();
+  //
+  //   $(".name").text(nameInput);
+  //
+  //   $("#letter").show();
+  //
+  //   event.preventDefault();
+  // });
+  $("form").submit(function(event) {
+    // Set starting score to 0
+    var score = 0;
+    // Tallies the point value from each quiz question
+    var result = parseInt($("input:radio[name=flavor]:checked").val());
+    score += result; // Re-sets score to score + result
+    result = parseInt($("input:radio[name=food]:checked").val());
+    score += result; // Re-resets score to score + result
+    result = parseInt($("input:radio[name=date]:checked").val());
+    score += result; // Re-resets score to score + result
+    result = parseInt($("input:radio[name=pet]:checked").val());
+    score += result; // Re-resets score to score + result
+    result = parseInt($("input:radio[name=foo]:checked").val());
+    score += result; // Re-resets score to score + result
+    // Calculate results //
+    // CHANGE THIS SO IT ECHOES BACK IN THE DOCUMENT
+    // RATHER THAN AS AN ALERT
+    if (score > 100) {
+      alert("Odessa, Ukraine");
+    }
+    else if (score > 50) {
+      alert("Berlin, Germany");
+    }
+    else {
+      alert("Reykjavik, Iceland");
+    }
     event.preventDefault();
   });
 });
